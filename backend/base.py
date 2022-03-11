41c143e0a51ac77683d7badfa1a720a55ecd13f4
Alex Young <younga6@oregonstate.edu>
2022-03-11 00:07:27 -0800

    update formatting and input box

--- backend/base.py
+++ backend/base.py
@@ -10,14 +10,14 @@ import urllib.parse
 import json
 
 api = Flask(__name__)
-#def get_results(filepath):
+
 @api.route('/results/<string:path>')
 def get_results(path: str):
     #path = './example.txt'
     file_path = urllib.parse.unquote(path)
     p = subprocess.Popen(f"scp {file_path} devcloud:deepfake/input/combined", shell=True, stdin=subprocess.PIPE,  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     time.sleep(3)
-    p2 = subprocess.Popen("scp devcloud:deepfake/input/combined/example.txt output.txt", shell=True, stdin=subprocess.PIPE,  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p2 = subprocess.Popen("scp devcloud:deepfake/output/submission-eff.csv output.txt", shell=True, stdin=subprocess.PIPE,  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     time.sleep(0.1)
     with open('output.txt') as f:
         lines = f.readlines()
--- src/App.js
+++ src/App.js
@@ -1,106 +1,120 @@
 /** @jsxImportSource @emotion/react */
 
-
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { useFilePicker } from "use-file-picker";
-import Spinner from './components/Spinner';
-import useResults from './hooks/useResults';
+import Spinner from "./components/Spinner";
+import useResults from "./hooks/useResults";
 // https://dev.to/nagatodev/how-to-connect-flask-to-reactjs-1k8i
 
-import { Global, css } from '@emotion/react'
+import { Global, css } from "@emotion/react";
 
 const globalStyles = css`
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400&display=swap');
+  @import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400&display=swap");
 
   body {
-    font-family: 'Source Sans Pro', sans-serif;
+    font-family: "Source Sans Pro", sans-serif;
     font-weight: 300;
     margin: 0;
   }
-    #upload {
-        position: absolute;
-        right: 0px;
-        top: 630px;
-        width: 400px;
-        height: 70px;
-        background-color: #66676d;
-        color: white;
-        font-family: "Georgia", serif;
-    }
+  #upload {
+    position: absolute;
+    right: 0px;
+    top: 630px;
+    width: 400px;
+    height: 70px;
+    background-color: #66676d;
+    color: white;
+    font-family: "Georgia", serif;
+  }
 
-    #upload:hover {
-        background-color: #dbdbdd;
-        cursor: pointer;
-        width: 400px;
-        height: 70px;
-        position: fixed;
-        color: black;
-    }
-    
-    #upload:active {
-        background-color: #dbdbdd;
-    }
-    
-    #copyfilepath {
-        position: absolute;
-        top: 650px;
-        right: 400px;
-        font-family: "Georgia", serif;
-        background-color: #66676d;
-        color: white;
-        width: 100px;
-        height: 30px;
-    }
+  #upload:hover {
+    background-color: #dbdbdd;
+    cursor: pointer;
+    width: 400px;
+    height: 70px;
+    position: fixed;
+    color: black;
+  }
 
-    #copyfilepath:hover {
-        background-color: #dbdbdd;
-        cursor: pointer;
-        width: 100px;
-        height: 30px;
-        position: fixed;
-        color: black;
-    }
-    
-    #input {
-        position: absolute;
-        top: 650px;
-        right: 490px;
-        font-family: "Georgia", serif;
-        height: 20px;
-    }
-    
-    #input:focus {
-        background-color: #dbdbdd;
-    }
+  #upload:active {
+    background-color: #dbdbdd;
+  }
 
+  #copyfilepath {
+    position: absolute;
+    top: 650px;
+    right: 400px;
+    font-family: "Georgia", serif;
+    background-color: #66676d;
+    color: white;
+    width: 100px;
+    height: 30px;
+  }
+
+  #copyfilepath:hover {
+    background-color: #dbdbdd;
+    cursor: pointer;
+    width: 100px;
+    height: 30px;
+    position: fixed;
+    color: black;
+  }
+
+  #input {
+    position: absolute;
+    top: 651px;
+    right: 500px;
+    font-family: "Georgia", serif;
+    height: 20px;
+  }
+
+  #input:focus {
+    background-color: #dbdbdd;
+  }
 `;
+
+const results = css`
+  margin-left: 40px;
+  color: white;
+`;
+
 function App() {
   const [openFileSelector, { filesContent, loading, errors }] = useFilePicker({
     multiple: false,
-    readAs: 'DataURL',
-    accept: '.txt, .mp4'
+    readAs: "DataURL",
+    accept: ".txt, .mp4",
   });
-  const [url, setUrl] = useState('');
-  const [output, setOutput] = useState('');
-  const [result, isLoading, error] = useResults(output)
+  const [url, setUrl] = useState("");
+  const [output, setOutput] = useState("");
+  const [result, isLoading, error] = useResults(output);
   if (errors.length > 0) return <p>Error!</p>;
-  if (loading) return <Spinner />
+  if (loading) return <Spinner />;
   return (
     <div>
       <Global styles={globalStyles} />
       <div>
-        <button id ="upload" onClick={() => openFileSelector()}>Upload</button>
-        <form onSubmit={(e) => {
-          e.preventDefault();
-          setOutput(encodeURIComponent(url));
-        }}>
+        <button id="upload" onClick={() => openFileSelector()}>
+          Upload
+        </button>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setOutput(encodeURIComponent(url));
+          }}
+        >
           <div id="box_upload">
-            <input id="input" value={url} onInput={e => setUrl(e.target.value)} />
-            <button id ="copyfilepath" type="submit">Copy file path</button>
+            <input
+              id="input"
+              value={url}
+              onInput={(e) => setUrl(e.target.value)}
+            />
+            <button id="copyfilepath" type="submit">
+              Copy file path
+            </button>
           </div>
         </form>
         {console.log(output)}
-        <div>{result}</div>
+        <div css={results}>{result}</div>
       </div>
     </div>
   );
